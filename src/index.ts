6d4c1a60408cad0734abced09d7141d0914b7b06
jacksonsr45 <jackson.severino.rocha@escola.pr.gov.br>
2023-07-12 10:16:23 -0300

    feat: add express setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-const message = "";
+import * as express from "express"
+import { Request, Response } from "express"
+
+const app = express()
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("Hello World!")
+})
+
+app.listen(3000, () => {
+    console.log("Example app listening on port 3000!")
+})
